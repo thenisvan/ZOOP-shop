747add51ee55a4082906dcc33e28efc72d8d7a86
Denco <thedenisivan@gmail.com>
2022-11-13 21:11:45 +0100

    initial implementation of discount calculator

--- src/Controller/MoneyMovementActions.java
+++ src/Controller/MoneyMovementActions.java
@@ -12,8 +12,8 @@ public class MoneyMovementActions {
         return 50;
     }
     public Double calculateDiscount(Double price, int points){
-        //this is where magic happen
-        return 0.15;
+//        return (price - price*(points/100) );
+        return 0.15; //15% off
     }
     public MoneyMovementActions(BuyProcess buyProcess) {
         this.BuyProcess = buyProcess;
@@ -22,15 +22,21 @@ public class MoneyMovementActions {
     public boolean startTransaction() {
         Buyer buyer = BuyProcess.getBuyer();
         Item item = BuyProcess.getItem();
+        Double subTotal = item.getItemPrice() * item.getAmount_toBuy();
+        Double discount = calculateDiscount(subTotal, buyer.getPoints());
+        Double discountedPrice = subTotal-discount;
+        int pointsFetched = calculatePoints(item, item.getAmount_toBuy());
 
-        if (buyer.getMoney() < item.getItemPrice() * item.getAmount_toBuy() /*TODO: implement discount based on user points*/) {
+//        TODO: ask how many points buyer wants to use
+
+        if (buyer.getMoney() < ( subTotal) /*TODO: implement discount based on user points*/) {
             System.out.println("You need more money. ");
             return false;
         }
 
         double newBalance = (buyer.getMoney() - item.getItemPrice() * item.getAmount_toBuy() /*TODO: implement discount based on user points*/);
         buyer.setBalance(newBalance);
-        buyer.setPoints(buyer.getPoints()*item.getAmount_toBuy());
+//        buyer.setPoints(buyer.getPoints()*item.getAmount_toBuy());
 
         SOUT_utils.delayMessage(1, String.format("Your money: $%.2f ", newBalance));
         return true;
